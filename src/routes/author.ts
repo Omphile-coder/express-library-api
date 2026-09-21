11795229be4ddf8dbdc122f5318326781683274e
Omphile Lucas <lucasomphile23@gmail.com>
2026-09-22 00:49:42 +0200

    Updated routes, add the PUT, DELETE and relationship mapping routes for authors

--- src/routes/author.ts
+++ src/routes/author.ts
@@ -1,45 +1,144 @@
 import { Router, Request, Response } from 'express';
 import { body, param, validationResult } from 'express-validator';
-import { createUser, getAllAuthors, getAuthorByID } from '../controllers/authors';
+import {
+    createUser,
+    deleteAuthor,
+    getAllAuthors,
+    getAuthorByID,
+    updateAuthor
+} from '../controllers/authors';
 
 const router = Router();
 
 let authors = [
-    { id: 1, name: "Professor Snape", email: "snape@hogwarts.com" },
-    {id: 2, name: "Professor Dumbledore", email: "dumbledore@hogwarts.com"},
-    
-]
+    {
+        id: 1,
+        name: "Professor Snape",
+        email: "snape@hogwarts.com"
+    },
+    {
+        id: 2,
+        name: "Professor Dumbledore",
+        email: "dumbledore@hogwarts.com"
+    }
+];
 
-//Get all Authors
+// Get all Authors
 router.get("/", getAllAuthors);
 
 // Get Author by ID
-router.get("/:id", [param("id").isInt().withMessage("ID must be an integer")], (req: Request, res: Response) => {
-    const errors = validationResult(req);
-    console.log(errors, "errors from express-validator middleware")
+router.get(
+    "/:id",
+    [param("id").isInt().withMessage("ID must be an integer")],
+    (req: Request, res: Response) => {
+        const errors = validationResult(req);
 
-    if (!errors.isEmpty()) {
-        return res.status(400).json({ errors: errors.array() });
+        console.log(errors, "errors from express-validator middleware");
+
+        if (!errors.isEmpty()) {
+            return res.status(400).json({
+                errors: errors.array()
+            });
+        }
+
+        getAuthorByID(req, res);
     }
-    
-    getAuthorByID(req, res)
+);
 
-});
+// Adding an Author
+router.post(
+    "/",
+    [
+        body("name")
+            .notEmpty()
+            .withMessage("Name is required"),
 
-// Adding a Author
-router.post("/", [
-    body("name").notEmpty().withMessage("Name is required"),
-    body("email").isEmail().withMessage("Must be a valid email address"),
-    
-], (req: Request, res: Response) => {
-    const errors = validationResult(req);
-    if (!errors.isEmpty()) {
-        return res.status(400).json({ errors: errors.array() })
+        body("email")
+            .isEmail()
+            .withMessage("Must be a valid email address")
+    ],
+    (req: Request, res: Response) => {
+        const errors = validationResult(req);
+
+        if (!errors.isEmpty()) {
+            return res.status(400).json({
+                errors: errors.array()
+            });
+        }
+
+        console.log(req.body, "request");
+
+        createUser(req, res);
     }
+);
 
-    console.log(req.body, 'request');
+// Update an Author
+router.put(
+    "/:id",
+    [
+        param("id")
+            .isInt()
+            .withMessage("ID must be an integer"),
 
-    createUser(req, res);
-        
-});
+        body("name")
+            .optional()
+            .notEmpty()
+            .withMessage("Name cannot be empty"),
+
+        body("email")
+            .optional()
+            .isEmail()
+            .withMessage("Must be a valid email address")
+    ],
+    (req: Request, res: Response) => {
+        const errors = validationResult(req);
+
+        if (!errors.isEmpty()) {
+            return res.status(400).json({
+                errors: errors.array()
+            });
+        }
+
+        updateAuthor(req, res);
+    }
+);
+
+// Delete Author
+router.delete(
+    "/:id",
+    [
+        param("id")
+            .isInt()
+            .withMessage("ID must be an integer")
+    ],
+    (req: Request, res: Response) => {
+        const errors = validationResult(req);
+
+        if (!errors.isEmpty()) {
+            return res.status(400).json({
+                errors: errors.array()
+            });
+        }
+
+        deleteAuthor(req, res);
+    }
+);
+
+// Get Books by Author (Sprint 4)
+// router.get(
+//     "/:id/books",
+//     [param("id").isInt().withMessage("ID must be an integer")],
+//     (req: Request, res: Response) => {
+//         const errors = validationResult(req);
+
+//         if (!errors.isEmpty()) {
+//             return res.status(400).json({
+//                 errors: errors.array()
+//             });
+//         }
+
+//         getBooksByAuthorId(req, res);
+//     }
+// );
+
 export default router;
