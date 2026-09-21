99159ecf82af292dae29368aee9d132b44c50cc6
Omphile Lucas <lucasomphile23@gmail.com>
2026-09-21 23:31:07 +0200

    Added error handling for 404/not found

--- src/middleware/error.ts
+++ src/middleware/error.ts
@@ -1,1 +1,11 @@
 import { Request, Response, NextFunction } from "express";
+import { error } from "node:console";
+
+export const notFoundHandler = (req: Request, res: Response, next: NextFunction) => {
+    res.status(404).json({
+        error: "Not Found",
+        message: `The requested URL ${req.originalUrl} was not found on this server.`
+    })
+
+
+ }
--- src/server.ts
+++ src/server.ts
@@ -5,6 +5,7 @@ import express, { Express } from 'express';
 import bodyParser from 'body-parser';
 import { loggerMiddleware } from './middleware/logger';
 import router from './routes/author';
+import { notFoundHandler } from './middleware/error';
 
 const app: Express = express();
 const PORT = process.env.PORT || 3000;
@@ -15,7 +16,9 @@ app.use(bodyParser.json())
 app.use(loggerMiddleware)
 
 // http://localhost:3000/v1/authors/
-app.use("/v1/authors", router)  
+app.use("/v1/authors", router);
+
+app.use(notFoundHandler);
 
 
 
