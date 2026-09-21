3b1fac27b2306761ca44a6df50e447420d6f809e
Omphile Lucas <lucasomphile23@gmail.com>
2026-09-21 23:56:16 +0200

    Made Authors array exportable,  so that books controller may also have access to it  and also finished the missing CRUD functions

--- src/controllers/authors.ts
+++ src/controllers/authors.ts
@@ -1,7 +1,8 @@
 import { Request, Response } from 'express';
+import { error } from 'node:console';
 
 
-let authors = [
+export let authors = [
     { id: 1, name: "Professor Snape", email: "snape@hogwarts.com" },
     { id: 2, name: "Professor Dumbledore", email: "dumbledore@hogwarts.com" },
     
@@ -32,3 +33,42 @@ export const createUser = (req: Request, res: Response) => {
 
     res.status(201).json(newAuthor);
 } 
+
+export const updateAuthor = (req: Request, res: Response) => {
+    const { id }: any = req.params;
+    const { name, email } = req.body;
+
+    const author = authors.find(a => a.id === parseInt(id, 10));
+
+    if (!author) {
+        return res.status(404).json({
+            error: "Not Found",
+            message: "Author not found"
+        });
+    }
+
+    if (name) {
+        author.name = name;
+    }
+
+    if (email) {
+        author.email = email;
+    }
+
+    return res.status(200).json(author);
+};
+
+export const deleteAuthor = (req: Request, res: Response) => { 
+    const { id } = req.params;
+    const index = authors.findIndex(a => a.id === parseInt(String(id), 10));
+
+    if (index === -1) {
+        return res.status(404).json({
+            error: "Not Found",
+            message: "Author not found"
+        })
+
+        authors.splice(index, 1);
+        res.status(204).send();
+    }
+}
