ae8c7d0940783e9d4a9793ad4d6c6ee98606e48c
Omphile Lucas <lucasomphile23@gmail.com>
2026-09-22 01:07:43 +0200

    Updated server.ts by importing the book router and register it with express

--- src/server.ts
+++ src/server.ts
@@ -1,28 +1,25 @@
-
-
 import express, { Express } from 'express';
-
 import bodyParser from 'body-parser';
 import { loggerMiddleware } from './middleware/logger';
-import router from './routes/author';
+import authorRouter from './routes/author'; 
+import bookRouter from './routes/book';     
 import { notFoundHandler } from './middleware/error';
 
 const app: Express = express();
 const PORT = process.env.PORT || 3000;
 
-app.use(express.json())
-app.use(bodyParser.json())
+app.use(express.json());
+app.use(bodyParser.json());
 
-app.use(loggerMiddleware)
+app.use(loggerMiddleware);
 
-// http://localhost:3000/v1/authors/
-app.use("/v1/authors", router);
+// API Routes
+app.use("/v1/authors", authorRouter);
+app.use("/v1/books", bookRouter); 
 
+// 404 Handler must come after routes
 app.use(notFoundHandler);
 
-
-
-
 app.listen(PORT, () => { 
-    console.log(`server is running on http://localhost:${PORT}`)
-})
+    console.log(`server is running on http://localhost:${PORT}`);
+});
